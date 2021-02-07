f4028c5cc995e7b2b359d309c0a845e64bec48bf
Joseph Roberts <59931152+Benjamin144@users.noreply.github.com>
2021-02-07 07:50:18 +0000

    Delete eny.py
    should have passed gitignore

--- eny.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "Ax8z]y}zd>yV}03k].OaYc<3KjE7R%")
-os.environ.setdefault("MONGO_URI", "mongodb://origin:tWist0faT4@myfirstcluster-shard-00-00.mu5yc.mongodb.net:27017,myfirstcluster-shard-00-01.mu5yc.mongodb.net:27017,myfirstcluster-shard-00-02.mu5yc.mongodb.net:27017/include_me?ssl=true&replicaSet=atlas-yqdttl-shard-0&authSource=admin&retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "include_me")
